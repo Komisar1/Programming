37e32f287ed272bdb5dfa825f796e5552c0eb6d7
Дима Аверкиев <144918714+Komisar1@users.noreply.github.com>
2023-11-29 16:31:01 +0300

    Add files via upload

---
+++ AverkievLB4/AverkievLB4/AverkievLB4.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int x, y;
+        bool flg1, flg2;
+        Console.WriteLine("Введите координату X: ");
+        flg1 = int.TryParse(Console.ReadLine(), out x);
+        Console.WriteLine("Введите координату Y: ");
+        flg2 = int.TryParse(Console.ReadLine(), out y);
+
+        if (flg1 && flg2)
+        {
+            if (x > 0 && y > 0)
+            {
+                Console.WriteLine("Точка находится в первом квадранте");
+            }
+            else if (x < 0 && y > 0)
+            {
+                Console.WriteLine("Точка находится во втором квадранте");
+            }
+            else if (x < 0 && y < 0)
+            {
+                Console.WriteLine("Точка находится в третьем квадранте");
+            }
+            else if (x > 0 && y < 0)
+            {
+                Console.WriteLine("Точка находится в четвертом квадранте");
+            }
+            else if (x == 0 && y != 0)
+            {
+                Console.WriteLine("Точка лежит на оси Y");
+            }
+            else if (x != 0 && y == 0)
+            {
+                Console.WriteLine("Точка лежит на оси X");
+            }
+            else
+            {
+                Console.WriteLine("Точка находится в начале координат");
+            }
+        }
+        else
+            Console.WriteLine("Введены некорректные данные");
+
+    }
+}
