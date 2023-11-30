ac47bf7b8ce1f06eb1701321b7eabf01faab66ce
Дима Аверкиев <144918714+Komisar1@users.noreply.github.com>
2023-11-30 13:21:14 +0300

    Add files via upload

---
+++ AverkievLB5/AverkievLB5/AverkievLB5.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        char vvod;
+        bool flg;
+        Console.Write("Введите латинский символ, обозначающий ось: ");
+        flg = char.TryParse(Console.ReadLine(), out vvod);
+        string axisName = "";
+
+        if (Char.IsLetter(vvod)) // проверка на то, что введённое значение является буквой
+        {
+            switch (vvod)
+            {
+                case 'x':
+                    Console.WriteLine("Латинский символ x соответствует оси абсцисс в терминах математики");
+
+                    break;
+                case 'y':
+                    Console.WriteLine("Латинский символ y соответствует оси ординат в терминах математики");
+                    break;
+                case 'z':
+                    Console.WriteLine("Латинский символ z соответствует оси аппликат в терминах математики");
+                    break;
+                default:
+                    Console.WriteLine("Оси, закодированной таким латинским символом - не существует");
+                    break;
+
+            }
+        }
+        else
+        {
+            Console.WriteLine("Введены некорректные данные");
+        }
+        Console.ReadKey(true);
+    }
+}
